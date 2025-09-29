5861c1e965b8777d9bb870ebafce73e1c8d58136
uosq <lboxwallytf2@gmail.com>
2025-09-29 01:16:05 -0300

    make this work in server side

--- src/main/java/navet/blazemilk/Blazemilk.java
+++ src/main/java/navet/blazemilk/Blazemilk.java
@@ -1,39 +1,12 @@
 package navet.blazemilk;
 
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.ItemUsage;
-import net.minecraft.item.Items;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.Hand;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Blazemilk implements ModInitializer {
 	public static final String MOD_ID = "blazemilk";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
-	public static boolean interact(MobEntity blaze, PlayerEntity player, Hand hand) {
-		if (blaze.getWorld().isClient) {
-			return false; // only run logic on the server
-		}
-
-		ItemStack stack = player.getStackInHand(hand);
-		if (stack.isOf(Items.BUCKET) && !blaze.isBaby()) {
-			// play sound
-			blaze.playSound(SoundEvents.ENTITY_BLAZE_HURT, 1.0f, 1.0f);
-
-			// give lava bucket instead of empty bucket
-			ItemStack lavaBucket = ItemUsage.exchangeStack(stack, player, Items.LAVA_BUCKET.getDefaultStack());
-			player.setStackInHand(hand, lavaBucket);
-
-			return true; // signal successful interaction
-		}
-		return false;
-	}
 
 	@Override
 	public void onInitialize() {
--- src/main/java/navet/blazemilk/mixin/ExampleMixin.java
+++ src/main/java/navet/blazemilk/mixin/ExampleMixin.java
@@ -4,13 +4,15 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.BlazeEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 
 import org.spongepowered.asm.mixin.Mixin;
-
-import navet.blazemilk.Blazemilk;
 
 @Mixin(BlazeEntity.class)
 public class ExampleMixin extends MobEntity {
@@ -20,8 +22,21 @@ public class ExampleMixin extends MobEntity {
 
 	@Override
 	public ActionResult interactMob(PlayerEntity player, Hand hand) {
-		if (Blazemilk.interact(this, player, hand))
-			return ActionResult.SUCCESS;
+		/* only run this in the server */
+		if (!this.getWorld().isClient) {
+			ItemStack stack = player.getStackInHand(hand);
+			if (stack.isOf(Items.BUCKET) && !this.isBaby()) {
+				// play sound
+				this.playSound(SoundEvents.ENTITY_BLAZE_HURT, 1.0f, 1.0f);
+
+				// give lava bucket instead of empty bucket
+				ItemStack lavaBucket = ItemUsage.exchangeStack(stack, player, Items.LAVA_BUCKET.getDefaultStack());
+				player.setStackInHand(hand, lavaBucket);
+
+				return ActionResult.SUCCESS;
+			}
+		}
+
 		return super.interactMob(player, hand);
 	}
 }
