f1a6c8cc53715088fb82407d050965ad705b255b
uosq <lboxwallytf2@gmail.com>
2025-09-29 00:46:26 -0300

    1.2 version(workson 1.21.8 now)

--- src/main/java/navet/blazemilk/Blazemilk.java
+++ src/main/java/navet/blazemilk/Blazemilk.java
@@ -14,29 +14,20 @@ import org.slf4j.LoggerFactory;
 
 public class Blazemilk implements ModInitializer {
 	public static final String MOD_ID = "blazemilk";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public static boolean interact(MobEntity blaze, PlayerEntity player, Hand hand) {
-		ItemStack itemStack = player.getStackInHand(hand);
-		if (itemStack.isOf(Items.BUCKET) && !blaze.isBaby()) {
-			player.playSound(SoundEvents.ENTITY_BLAZE_HURT, 1.0f, 1.0f);
-			ItemStack itemStack2 = ItemUsage.exchangeStack(itemStack, player, Items.LAVA_BUCKET.getDefaultStack());
-			player.setStackInHand(hand, itemStack2);
-			return true;
+		ItemStack stack = player.getStackInHand(hand);
+		if (stack.isOf(Items.BUCKET) && !blaze.isBaby()) {
+			blaze.playSound(SoundEvents.ENTITY_BLAZE_HURT, 1.0f, 1.0f);
+			ItemUsage.exchangeStack(stack, player, Items.BUCKET.getDefaultStack());
+			player.setStackInHand(hand, stack);
 		}
 		return false;
-    }
+	}
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Blazemilk should be loaded!");
+		LOGGER.info("Blazemilk loaded!");
 	}
 }
--- src/main/java/navet/blazemilk/mixin/ExampleMixin.java
+++ src/main/java/navet/blazemilk/mixin/ExampleMixin.java
@@ -4,25 +4,28 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.entity.mob.BlazeEntity;
 import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUsage;
+import net.minecraft.item.Items;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
+
 import org.spongepowered.asm.mixin.Mixin;
 
 import navet.blazemilk.Blazemilk;
 
 @Mixin(BlazeEntity.class)
-public class BlazeMixin extends MobEntity {
-
-	protected BlazeMixin(EntityType<? extends MobEntity> entityType, World world) {
+public class ExampleMixin extends MobEntity {
+	protected ExampleMixin(EntityType<? extends MobEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
 	@Override
 	public ActionResult interactMob(PlayerEntity player, Hand hand) {
-		if (Blazemilk.interact(this, player, hand)) {
+		if (Blazemilk.interact(this, player, hand))
 			return ActionResult.SUCCESS;
-		}
 		return super.interactMob(player, hand);
 	}
 }
